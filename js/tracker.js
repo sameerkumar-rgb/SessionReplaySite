9fcb6f9b3526813bfcfd267dee11b7b46736e228
sameerkumar-rgb <sameer.kumar@gainserv.com>
2026-01-13 10:48:30 +0530

    Update tracker.js

--- js/tracker.js
+++ js/tracker.js
@@ -2,7 +2,6 @@
 // Add your session replay SDK, analytics, or tracking code here
 
 
-(function(u,z,a){if(u["a_id"]){return;}u["a_id"]=a;u["dm"]=u.location.hostname;u.uzera=u.uzera||[];u.uzera.q=u.uzera.q||[];['identify','trackEvent'].forEach(function(m){u.uzera[m]=function(){u.uzera.q.push([m,arguments]);};});var s=z.createElement("script");s.async=true;s.src="https://assets.gainserv.in/wrapper.dev.min.js";var t=z.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);})(window,document,"Q4A0767BAJ");
-
+(function(u,z,a){if(u["a_id"]){return;}u["a_id"]=a;u["dm"]=u.location.hostname;u.uzera=u.uzera||[];u.uzera.q=u.uzera.q||[];['identify','trackEvent'].forEach(function(m){u.uzera[m]=function(){u.uzera.q.push([m,arguments]);};});var s=z.createElement("script");s.async=true;s.src="https://assets.gainserv.in/wrapper.dev.min.js";var t=z.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);})(window,document,"AS4QBEN0BG");
   console.log('Tracker loaded on:');
 
